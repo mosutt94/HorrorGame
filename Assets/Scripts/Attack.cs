9259275c3659aa122f2f6f1c9f2691001dde6a29
mosutt94 <ms6880@nyu.edu>
2016-05-12 22:10:52 -0400

    Add files via upload

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+	private AudioSource audioSource;
+	public GameObject clown;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent <AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Scare")){
+			audioSource.Play ();
+
+		}
+
+
+
+	}
+
+}
